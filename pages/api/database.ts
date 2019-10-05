dee2e6f0c4ea34a1f15a43431e67251aeae2ddda
Vigo Vlugt <vigovlugt@gmail.com>
2019-10-05 22:04:30 +0200

    Fix formatting

--- pages/api/database.ts
+++ pages/api/database.ts
@@ -3,17 +3,15 @@ import getConnection from "../../lib/database";
 import { RowDataPacket } from "mysql2/promise";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    try{
-        const con = await getConnection();
-        const {sql} = JSON.parse(req.body);
-        console.log(sql)
-        const [results,tableInfo] = await con.query<RowDataPacket[]>(sql,[]);
+  try {
+    const con = await getConnection();
+    const { sql } = JSON.parse(req.body);
+    console.log(sql);
+    const [results, tableInfo] = await con.query<RowDataPacket[]>(sql, []);
 
-        res.json(results)
-    }
-    catch (error){
-        console.error(error)
-        return {error}
-    }
-
-}
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    return { error };
+  }
+};
